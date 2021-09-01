e2f2df0cceecd811483d81f09ba89032156209d7
Gera Bernal <gerardo.bernal@uanl.edu.mx>
2021-08-31 20:01:19 -0500

    Agregando script pokemon2.py

---
+++ pokemon2.py
@@ -0,0 +1,19 @@
+import requests
+#
+def get_pokemons(url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
+    args = {'offset':offset} if ofsset else {}
+
+    response = response.get(url, params=args)
+    if response.status_code == 200:
+		payload = response.json()
+		results = payload.get('results',[])
+
+		if results:
+			for pokemon in results:
+				name = pokemon['name']
+				print(name)
+        next = input("¿continuar listando' [Y/N]").lower()
+        if next = y
+            get_pokemons(offset=offset+20)
+if __name__ == '__main__':
+    get_pokemons()
